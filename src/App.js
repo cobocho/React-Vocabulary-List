4cbe344c88c851dcbacdc4d2409d77f425851c13
Cobocho <99083803+Cobocho@users.noreply.github.com>
2023-01-23 03:21:06 +0900

    feat: 헤더 영역 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Fragment } from "react";
 import "./App.css";
 
+import Header from "./Components/Header/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const VocaHeader = styled.h1`
+  font-size: 72px;
+  color: black;
+  letter-spacing: -0.05em;
+`;
+
+const Header = () => {
+  return <VocaHeader>MY VOCA</VocaHeader>;
+};
+
+export default Header;
