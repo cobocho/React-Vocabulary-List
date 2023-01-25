cf85ec310268c06b3528a4aa88bbb1c3e5174ebd
Cobocho <99083803+Cobocho@users.noreply.github.com>
2023-01-26 05:15:57 +0900

    feat: NotFound 페이지 구현

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,5 +1,25 @@
+import Card from "../Components/UI/Card";
+
+const { default: styled } = require("styled-components");
+
+const NotFoundText = styled(Card)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 500px;
+
+  h1 {
+    font-size: 72px;
+  }
+`;
+
 const NotFound = () => {
-  return <h1>Not Found</h1>;
+  return (
+    <NotFoundText>
+      <h1>Not Found</h1>
+    </NotFoundText>
+  );
 };
 
 export default NotFound;
