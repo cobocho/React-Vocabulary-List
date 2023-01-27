14a0ab41a279502fd80d11fc619f70d33b76051b
Cobocho <99083803+Cobocho@users.noreply.github.com>
2023-01-28 01:12:31 +0900

    feat: progressive bar 구현

---
+++ src/Components/TestWord/ProgressBar.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+const ProgressBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    font-weight: 500;
+    font-size: 14px;
+    color: #939393;
+    margin-bottom: 4px;
+  }
+`;
+
+const ProgressBarBackground = styled.div`
+  width: 240px;
+  height: 20px;
+  border-radius: 12px;
+  background-color: #eeeeee;
+`;
+
+const ProgressBarFill = styled(ProgressBarBackground)`
+  width: ${(props) => props.percent};
+  height: 20px;
+  border-radius: 12px;
+  background-color: #000000;
+`;
+
+const ProgressBar = ({ progress, entire }) => {
+  const fill = (progress / entire).toFixed(2) * 100 + "%";
+
+  return (
+    <ProgressBox>
+      <p>
+        {progress} / {entire}
+      </p>
+      <ProgressBarBackground>
+        <ProgressBarFill percent={fill} />
+      </ProgressBarBackground>
+    </ProgressBox>
+  );
+};
+
+export default ProgressBar;
